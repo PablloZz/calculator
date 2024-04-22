2544c1b21184fd01c47f68ea2f354a176a5d1a4c
Paul <84803938+PablloZz@users.noreply.github.com>
2024-04-22 11:24:12 +0300

    feat: Implement result display and calculation
    Add a shouldCalculateResult variable to update the expression string
    and determine if calculate result;
    Refactor the populateDisplay function for cleaner code.
    Move some code from the getExpressionString and populateExpression
    functions to a handleExpression function for readability;

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const buttons = document.querySelectorAll("button");
 const expressionOutput = document.querySelector(".expression");
+const expressionResultOutput = document.querySelector(".result");
+const expressionButtons = document.querySelectorAll(".expression-button");
+const equalButton = document.querySelector(".equals-button");
 
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
@@ -9,6 +11,7 @@ const divide = (a, b) => a / b;
 let operator = "";
 let firstOperand = "";
 let secondOperand = "";
+let shouldCalculateResult = false;
 
 function operate(a, operator, b) {
   a = Number(a);
@@ -33,35 +36,64 @@ function updateExpressionParts(newPart) {
     firstOperand = firstOperand === "0" ? newPart : firstOperand + newPart;
   }
 
-  if (!firstOperand && !isNumber) {
-    firstOperand = "0";
-  }
+  if (!firstOperand && !isNumber) firstOperand = "0";
 
   if (operator && isNumber) {
     secondOperand = secondOperand === "0" ? newPart : secondOperand + newPart;
   }
 
-  if (!isNumber) {
-    operator = newPart;
-  }
+  if (!isNumber && newPart !== "=") operator = newPart;
+  if (newPart === "=" && !secondOperand) shouldCalculateResult = false;
+  if (newPart === "=" && secondOperand) shouldCalculateResult = true;
 }
 
-function getExpressionString(newPart) {
-  updateExpressionParts(newPart);
+function getExpressionString() {
   let expression = firstOperand;
 
   if (operator) expression += ` ${operator} `;
   if (secondOperand) expression += secondOperand;
+  if (shouldCalculateResult) expression += " =";
 
   return expression;
 }
 
-function populateDisplay(event) {
-  const buttonValue = event.target.textContent;
-  const expression = getExpressionString(buttonValue);
+function populateExpression() {
+  const expression = getExpressionString();
   expressionOutput.textContent = expression;
 }
 
-buttons.forEach((button) => {
-  button.addEventListener("click", populateDisplay);
+function handleExpressionResult() {
+  let expressionResult = "";
+
+  if (shouldCalculateResult) {
+    expressionResult = operate(firstOperand, operator, secondOperand);
+    firstOperand = expressionResult;
+    operator = "";
+    secondOperand = "";
+    shouldCalculateResult = false;
+  }
+
+  return expressionResult;
+}
+
+function populateExpressionResult() {
+  const result = handleExpressionResult();
+  expressionResultOutput.textContent = result;
+}
+
+function populateDisplay() {
+  populateExpression();
+  populateExpressionResult();
+}
+
+function handleExpression(event) {
+  const buttonValue = event.target.textContent;
+  updateExpressionParts(buttonValue);
+  populateDisplay();
+}
+
+expressionButtons.forEach((button) => {
+  button.addEventListener("click", handleExpression);
 });
+
+equalButton.addEventListener("click", handleExpression);
